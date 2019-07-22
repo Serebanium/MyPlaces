73ced06ed4a8ef854b6fe6919dd8b896953bb528
Сергей Иванов <serebanium@icloud.com>
2019-07-22 10:27:07 +0300

    Image Picker Controller

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,72 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Сергей Иванов on 21/07/2019.
+//  Copyright © 2019 topMob. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       tableView.tableFooterView = UIView()
+    }
+    
+    // MARK: TableviewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Камера", style: .default) { _ in
+                self.choseImahePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Галерея", style: .default) { _ in
+                 self.choseImahePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Отмена", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+  
+}
+
+// MARK: Tex field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    // Прячем клавиатуру по Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+// Mark: Work with image
+
+extension NewPlaceViewController {
+    func choseImahePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true // позвволяет отредактировать рисунок перед выбором
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+}
